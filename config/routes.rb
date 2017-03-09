daff92cb8e4bbe9ae72de1a69cc916b28802f19a
justin skord <jskord@gmail.com>
2017-03-08 20:42:20 -0600

    got spills plotted

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-
-  namespace :api do
-    namespace :v1 do
-      resources :search
-      get '/spills' => 'spills#index'
-    end
-  end
+  
   get '/' => 'pages#index'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
@@ -13,5 +7,10 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   get '/map' => 'pages#map'
+
+  
+  get '/json/all' => 'maps#json_index'
+  get '/json/sort_by_date' => 'maps#json_sort_by_date'
+  get '/json/show' => 'maps#json_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
