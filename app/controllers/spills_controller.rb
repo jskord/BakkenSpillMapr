a46e9fe871e39914831c6c2e98cac6b66b2c4e5b
justin skord <jskord@gmail.com>
2017-03-09 16:27:37 -0600

    got show page working

--- app/controllers/spills_controller.rb
+++ app/controllers/spills_controller.rb
@@ -2,4 +2,10 @@ class SpillsController < ApplicationController
   def index
     @spills = Spill.all
   end 
+
+  def show
+    spill_id = params[:id]
+    @spill = Spill.find_by(id: spill_id)
+    render 'show.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   get '/json/all' => 'maps#json_index'
   get '/json/sort_by_date' => 'maps#json_sort_by_date'
   get '/json/show' => 'maps#json_show'
+
+  get '/spills/:id' => 'spills#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
