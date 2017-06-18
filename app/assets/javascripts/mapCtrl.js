2a35f908d75a5b866de4ea28ec9bafa318e0bf46
justin skord <jskord@gmail.com>
2017-06-18 16:17:35 -0500

    starting up angular again

--- app/assets/javascripts/mapCtrl.js
+++ app/assets/javascripts/mapCtrl.js
@@ -1,24 +1,8 @@
 (function() {
   'use strict';
-  angular.module('app').controller('mapCtrl',
-    ['$scope','$http', '$httpParamSerializerJQLike', '$timeout',
-      function($scope, $http, $httpParamSerializerJQLike, $timeout) {
-        $scope.query = {};
-        $scope.data = [];
-        $scope.map;
-        $scope.searchResults = "";
-        $scope.markers = [];
-
-    // delay loading of google map
-        $timeout(function() {
-          var uluru = {lat: 41.0906369, lng: -85.0814343};
-          $scope.map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-          });
-        }, 100);
-
-        window.$scope = $scope;
-      }]);
+  angular.module('app').controller('mapCtrl', ['$scope', function($scope) {
+    $scope.greeting = "hello";
+  }]);
 
 })();
+
