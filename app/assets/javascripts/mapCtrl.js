011717da4978cc553badefa38dc6939f03c9e049
justin skord <jskord@gmail.com>
2017-06-18 16:34:25 -0500

    input box

--- app/assets/javascripts/mapCtrl.js
+++ app/assets/javascripts/mapCtrl.js
@@ -2,6 +2,8 @@
   'use strict';
   angular.module('app').controller('mapCtrl', ['$scope', function($scope) {
     $scope.greeting = "hello";
+    var startDate = "2012";
+    var endDate = "2013";
   }]);
 
 })();
